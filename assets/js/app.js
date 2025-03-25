31996c6c61b70eab6f8bb72a1b5b5af1a9eef370
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-03-25 16:36:15 +0530

    add roadmap

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,6 +37,35 @@ const x = setInterval(() => {
         counter.textContent = "00:00:00:00";
     }
 }, 1000);
+document.querySelectorAll('.accordion-item').forEach(item => {
+    const accBtn = item.querySelector('.accordion-btn');
+    accBtn.addEventListener('click', () => toggleAccordion(item));
+});
+
+function toggleAccordion(activeItem) {
+    const accordionItems = document.querySelectorAll('.accordion-item');
+
+    accordionItems.forEach(item => {
+        const content = item.querySelector('.accordion-text');
+        const arrow = item.querySelector('img');
+
+        if (item === activeItem) {
+            item.classList.toggle('active');
+            content.classList.toggle('-mt-56');
+         
+            arrow.classList.toggle('rotate-180');
+            arrow.src = item.classList.contains('active') 
+                ? './assets/images/svg/open-acc.svg' 
+                : './assets/images/svg/plus.svg';
+        } else {
+            item.classList.remove('active');
+            content.classList.add('-mt-56');
+           
+            arrow.classList.remove('rotate-180');
+            arrow.src = './assets/images/svg/plus.svg';
+        }
+    });
+}
 
 $('.tweet-slider').slick({
     dots: false,
@@ -74,3 +103,4 @@ $('.tweet-slider').slick({
         }
     ]
 });
+// accordian
