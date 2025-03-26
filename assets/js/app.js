59509eb40a313f1a1f20312417de7fd687d1ec31
Ankush Dhull <apple@Apples-MacBook-Air.local>
2025-03-26 14:06:16 +0530

    slider set

--- assets/js/app.js
+++ assets/js/app.js
@@ -68,9 +68,9 @@ $('.tweet-slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
-    prevArrow: '.right-arrow',
-    nextArrow: '.left-arrow',
+    autoplaySpeed: 3000,
+    prevArrow: '.left-arrow',
+    nextArrow: '.right-arrow',
     responsive: [
         {
             breakpoint: 1024,
@@ -103,25 +103,17 @@ $('.team-slider').slick({
     speed: 800,
     slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 3000,
+    // autoplay: true,
     prevArrow: '.team-left-arrow',
     nextArrow: '.team-right-arrow',
     responsive: [
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
                 slidesToShow: 2,
-                slidesToScroll: 2
+                slidesToScroll: 1,
+                infinite: true,
             }
         },
         {
